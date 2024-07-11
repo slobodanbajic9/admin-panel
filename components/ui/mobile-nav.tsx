e0c8113f4f8e4ec7a50f9ed8fd07b244458e26ac
slobodanbajic9 <slobodan.bajic9@gmail.com>
2024-07-11 13:27:57 +0200

    display users from postgress db

--- components/ui/mobile-nav.tsx
+++ components/ui/mobile-nav.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { Button } from "./button";
 import { Badge } from "./badge";
@@ -10,8 +11,6 @@ import {
   Package2,
   ShoppingCart,
   Users,
-  Search,
-  CircleUser,
 } from "lucide-react";
 import {
   Card,
@@ -20,10 +19,11 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { navItems } from "./sidebar";
 
 export default function MobileNav() {
+  const pathname = usePathname();
   return (
     <Sheet>
       <SheetTrigger asChild>
@@ -34,45 +34,17 @@ export default function MobileNav() {
       </SheetTrigger>
       <SheetContent side="left" className="flex flex-col">
         <nav className="grid gap-2 text-lg font-medium">
-          <Link
-            href="#"
-            className="flex items-center gap-2 text-lg font-semibold">
-            <Package2 className="h-6 w-6" />
-            <span className="sr-only">Acme Inc</span>
-          </Link>
-          <Link
-            href="#"
-            className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground">
-            <Home className="h-5 w-5" />
-            Dashboard
-          </Link>
-          <Link
-            href="#"
-            className="mx-[-0.65rem] flex items-center gap-4 rounded-xl bg-muted px-3 py-2 text-foreground hover:text-foreground">
-            <ShoppingCart className="h-5 w-5" />
-            Orders
-            <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
-              6
-            </Badge>
-          </Link>
-          <Link
-            href="#"
-            className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground">
-            <Package className="h-5 w-5" />
-            Products
-          </Link>
-          <Link
-            href="#"
-            className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground">
-            <Users className="h-5 w-5" />
-            Customers
-          </Link>
-          <Link
-            href="#"
-            className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground">
-            <LineChart className="h-5 w-5" />
-            Analytics
-          </Link>
+          {navItems.map((item, index) => (
+            <Link
+              key={index}
+              href={item.href}
+              className={`flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary ${
+                pathname === item.href ? "bg-muted text-primary" : ""
+              }`}>
+              {item.icon}
+              {item.label}
+            </Link>
+          ))}
         </nav>
         <div className="mt-auto">
           <Card>
